b2c21a8a80e7b5255e045d55e85c2a6b0730b913
Jiaxin Shan <seedjeffwan@gmail.com>
2019-02-21 16:26:44 -0800

    Fix authentication issue in copy_results.sh

--- src/benchmark/test/copy_results.sh
+++ src/benchmark/test/copy_results.sh
@@ -1,7 +1,7 @@
 
 NAMESPACE=$1
 
-NFS_POD_NAME=$(kubectl get pods -l role=kubebench-nfs -o jsonpath="{.items[0].metadata.name}")
+NFS_POD_NAME=$(kubectl --kubeconfig=$BENCHMARK_DIR/kubeconfig get pods -l role=kubebench-nfs -o jsonpath="{.items[0].metadata.name}")
 
 kubectl cp ${NAMESPACE}/${NFS_POD_NAME}:/exports/kubebench/config ${BENCHMARK_DIR}/output/config
 kubectl cp ${NAMESPACE}/${NFS_POD_NAME}:/exports/kubebench/experiments ${BENCHMARK_DIR}/output/experiments
