6b190d4fd0ae40d930b1d07ff21c53c3d3c144fa
Jiaxin Shan <seedjeffwan@gmail.com>
2019-04-15 17:18:29 -0700

    Remove job config copy and use s3 instead

--- src/benchmark/test/setup_job_config.sh
+++
@@ -1,8 +0,0 @@
-NAMESPACE=$1
-
-NFS_POD_NAME=$(kubectl --kubeconfig=${BENCHMARK_DIR}/kubeconfig get pods -l role=kubebench-nfs -o jsonpath="{.items[0].metadata.name}")
-
-kubectl --kubeconfig=${BENCHMARK_DIR}/kubeconfig cp /tmp/mpi-job-1.yaml ${NAMESPACE}/${NFS_POD_NAME}:/exports/kubebench/config/mpi/mpi-job-1.yaml
-kubectl --kubeconfig=${BENCHMARK_DIR}/kubeconfig cp /tmp/mpi-job-2.yaml ${NAMESPACE}/${NFS_POD_NAME}:/exports/kubebench/config/mpi/mpi-job-2.yaml
-kubectl --kubeconfig=${BENCHMARK_DIR}/kubeconfig cp /tmp/mpi-job-4.yaml ${NAMESPACE}/${NFS_POD_NAME}:/exports/kubebench/config/mpi/mpi-job-4.yaml
-kubectl --kubeconfig=${BENCHMARK_DIR}/kubeconfig cp /tmp/mpi-job-8.yaml ${NAMESPACE}/${NFS_POD_NAME}:/exports/kubebench/config/mpi/mpi-job-8.yaml
